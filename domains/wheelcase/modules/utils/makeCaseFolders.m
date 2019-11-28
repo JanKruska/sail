ddfd873f5dc0414419b9c03c24cc6e863d030255
Jan Kruska <mortagh@download-privat.de>
2019-11-28 11:59:59 +0100

    moved utils

--- domains/wheelcase/modules/utils/makeCaseFolders.m
+++
@@ -1,7 +0,0 @@
-function makeCaseFolders(d)
-for iCase = 1:d.nCases
-    openFoamFolder = [d.openFoamFolder 'case' int2str(iCase+d.caseStart-1) '/'];
-    system(['mkdir -p ' openFoamFolder]);
-    system(['cp -af ' d.baseFolder '. ' openFoamFolder]);
-end
-end
--- domains/wheelcase/modules/utils/rmCaseFolders.m
+++
@@ -1,6 +0,0 @@
-function rmCaseFolders(d)
-for iCase = 1:d.nCases
-    openFoamFolder = [d.openFoamFolder 'case' int2str(iCase+d.caseStart-1) '/'];
-    system(['rm -rf ' openFoamFolder]);
-end
-end
